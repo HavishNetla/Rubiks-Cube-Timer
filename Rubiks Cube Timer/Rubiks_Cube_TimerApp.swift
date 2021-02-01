292d6eeab4137c84ad55b016ccfc2178aa8ecc13
Havish Netla <netlahavish@gmail.com>
2021-02-01 16:23:19 -0500

    Initial Commit

---
+++ Rubiks Cube Timer/Rubiks_Cube_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rubiks_Cube_TimerApp.swift
+//  Rubiks Cube Timer
+//
+//  Created by Havish Netla on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Rubiks_Cube_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
